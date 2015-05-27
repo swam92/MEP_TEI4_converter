977d0c8981c6d8ee0d1e0ac87ff046c314754c0f
swam92 <svw223@nyu.edu>
2015-05-27 00:13:21 -0400

    scaffolding project

---
+++ Converter.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Converter{
+
+	public static void main(String[] args) throws IOException{
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+	}
+
+}
